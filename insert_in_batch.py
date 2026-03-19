7bcf0ff307790f46a492af819ac583da75fe21d4
Zemplenyi Boldizsar <boldizsar.zemplenyi@aumovio.com>
2026-03-19 20:18:45 +0100

    feat: insert in batch

---
+++ insert_in_batch.py
@@ -0,0 +1,14 @@
+import time
+
+BATCH_SIZE=100
+
+def insert_in_batch(collection, objs, name: str):
+    for i in range(0, len(objs), BATCH_SIZE):
+        batch = objs[i:i+BATCH_SIZE]
+        response = collection.data.insert_many(batch)
+
+        print(f"Insertion complete with {len(response.all_responses)} objects for '{name}' collection.")
+        print(f"Insertion errors: {len(response.errors)}.")
+
+        if i + BATCH_SIZE < len(objs):
+            time.sleep(60)
